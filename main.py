9034ea18455246d6cf8d38af71eac11531f3c283
Василиса Андросова <and3vasya@yandex.ru>
2024-07-01 22:02:24 +0300

    homework 12_1 done

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os.path
 
-from src.decorators import log
+from src.external_api import get_transaction_amount
 from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 from src.processing import get_dict_by_key_state, get_dict_date_dicrease
+from src.utils import get_data_financial_transactions
 from src.widget import get_date_string, get_mask_card_or_account
 
 print(get_mask_card_or_account("Maestro 1596837868705199"))
@@ -118,9 +119,11 @@ for card_number in card_number_generator(1, 5):
     print(card_number)
 
 
-@log(filename="mylog.txt")
-def my_function(x: int, y: int) -> int:
-    return x + y
+current_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(current_dir, "data", "operations.json")
+transactions = get_data_financial_transactions(file_path)
 
 
-my_function(1, 2)
+for transaction in transactions:
+    rub_amount = get_transaction_amount(transaction)
+    print(f"Transaction amount in RUB: {rub_amount}")
