3f4b3ff5b1311aa52783252c3d07813e99c66ded
Василиса Андросова <and3vasya@yandex.ru>
2024-07-24 17:26:24 +0300

    def read_xlsx_file has been fixed

--- src/reader.py
+++ src/reader.py
@@ -17,8 +17,19 @@ def read_csv_file(tr_reader: Any) -> Any:
 def read_xlsx_file(df: Any) -> Any:
     """Функция реализовывает считывание финансовых операций из XLSX-файла"""
     workbook = openpyxl.load_workbook("data/transactions_excel.xlsx")
-    sheet = workbook.active
     dict_xlsx = []
-    for row in sheet.iter_rows(values_only=True):
-        dict_xlsx.append(row)
+    for column_name, column_data in df.iterrows():
+        m = {
+            "id": column_data["id"],
+            "state": column_data["state"],
+            "date": column_data["date"],
+            "amount": column_data["amount"],
+            "currency_name": column_data["currency_name"],
+            "currency_code": column_data["currency_code"],
+            "from": column_data["from"],
+            "to": column_data["to"],
+            "description": column_data["description"],
+        }
+        dict_xlsx.append(m)
+
     return dict_xlsx
